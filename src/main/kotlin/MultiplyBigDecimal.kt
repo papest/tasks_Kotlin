b7e42689897b65899efbc2dca3d371e4dbbd7d09
Epap <peia@inbox.ru>
2023-03-21 16:50:58 +0300

    BigDecimal

---
+++ src/main/kotlin/MultiplyBigDecimal.kt
@@ -0,0 +1,8 @@
+/**
+ * Multiplication
+ * Write a program that takes two strings as input, converts them to BigDecimal numbers, multiplies them, and prints the result.
+ */
+
+fun main() {
+    println(readln().toBigDecimal().multiply(readln().toBigDecimal()))
+}
